0fa21f8a8f1807b8128c62f084c862f4c8995ea5
Fabricio-Tomas <fabritomas25@gmail.com>
2022-08-27 18:04:16 -0300

    imports failure

--- src/ui/Window.java
+++ src/ui/Window.java
@@ -5,7 +5,6 @@ import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
